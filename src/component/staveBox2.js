287f03a3825f849c1a90cb2e6d4731c1c39696ea
memrye <bloab321@gmail.com>
2026-01-19 13:08:27 +0000

    all html elements associated with a staveBox are stored inside of an el object

--- src/component/staveBox2.js
+++ src/component/staveBox2.js
@@ -7,22 +7,30 @@ export class StaveBox2 {
      * @param {Workspace} workspace - The parent workspace for the staveBox.
      * @param {string[]} tuning - The staveBox's inital tuning.
      * @param {object} [staveBoxOptions] - Optional staveBox options.
-     * @param {number} [staveBoxOptions.length] - Length of stavebox.
+     * @param {number} [staveBoxOptions.length] - Length of staveBox.
      * @param {[]} [staveBoxOptions.clonedCellArray] - Array of cell values to copy from.
      */
     constructor(workspace, tuning = ['E', 'A', 'D', 'G', 'B', 'e'], staveBoxOptions = { length: 24,  clonedCellArray: [] }) {
         
         const options = staveBoxOptions;
-        this.workspace = workspace;
+        this.parentWorkspace = workspace;
         this.tuning = tuning;
 
+        /** @member {Object} el - Contains all HTML Elements associated with this object */
+        this.el = {};
+
+        /** @member {Object[]} cellArray - A 2d array containing all the staveBox's grid's values and indexes.  */
         this.cellArray = initCellArray(options.length, tuning.length, options.clonedCellArray);
 
-        
-
+        this.el.baseContainer = document.createElement('div');
+        this.el.baseContainer.classList.add('prototypeContainer','stave');
+        this.parentWorkspace.el.appendChild(this.el.baseContainer);
 
     }
 
+    getRootContainer(){
+        return this.el.baseContainer;
+    }
 }
 
 /**
